07102ec2b33782d11668bc51845de4df0a1eb517
ashutoshray-dev <ashukr.info1@gmail.com>
2026-03-25 23:29:46 +0530

    fetching_all_threads_backend

--- chatbot_backend.py
+++ chatbot_backend.py
@@ -4,11 +4,11 @@ from langchain_core.messages import HumanMessage, BaseMessage
 from langchain_ollama import ChatOllama
 from dotenv import load_dotenv
 load_dotenv()
-import operator
-from langgraph.checkpoint.memory import MemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
+import sqlite3
 from langgraph.graph.message import add_messages
 class ChatState(TypedDict):
-    messages: Annotated[list[BaseMessage], operator.add]
+    messages: Annotated[list[BaseMessage], add_messages]
 
 model = ChatOllama(model='qwen2.5:3b')
 def chat_node(state:ChatState):
@@ -16,7 +16,9 @@ def chat_node(state:ChatState):
     # print(messages)
     response = model.invoke(messages)
     return {'messages': [response]}
-checkpointer = MemorySaver()
+
+conn = sqlite3.connect(database='langraph_chatbot.db', check_same_thread=False)
+checkpointer = SqliteSaver(conn=conn)
 graph = StateGraph(ChatState)
 graph.add_node('chat', chat_node)
 graph.add_edge(START, 'chat')
@@ -31,3 +33,10 @@ chatbot = graph.compile(checkpointer=checkpointer)
 #     config = {'configurable':{'thread_id':thread_id}}
 #     response = chatbot.invoke({'messages': [HumanMessage(content=user_message)]}, config=config)
 #     print('AI: ', response['messages'][-1].content)
+
+def retrieve_threads_list():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+
+    return list(all_threads)
